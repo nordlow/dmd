7cfe2a09d5fbe49559a882a47d3a7f127bf6d547
Per Nordlöw <per.nordlow@gmail.com>
2014-02-28 12:11:43 +0100

    Generate/Ignore Exuberant Ctags files tags

---
+++ test/fail_compilation/fail3882.d
@@ -0,0 +1,26 @@
+/*
+  TEST_OUTPUT:
+  ---
+  fail_compilation/fail3882.d(25,17): Warning: Call to strictly pure function fail3882.strictlyPure!int.strictlyPure discards return value, prepend a cast(void) if intentional
+  ---
+*/
+
+@safe pure nothrow void strictVoidReturn(T)(T x)
+{
+}
+
+@safe pure nothrow void nonstrictVoidReturn(T)(ref T x)
+{
+}
+
+@safe pure nothrow T strictlyPure(T)(T x)
+{
+    return x*x;
+}
+
+void main(string args[]) {
+    int x = 3;
+    strictVoidReturn(x);
+    nonstrictVoidReturn(x);
+    strictlyPure(x);
+}
