04eca17267f4df22a0e1295e89c342b52af1c852
Walter Bright <walter@walterbright.com>
2016-09-26 22:35:58 -0700

    add 'scope' support to 'this'

--- src/func.d
+++ src/func.d
@@ -2399,8 +2399,14 @@ extern (C++) class FuncDeclaration : Declaration
                 if (type.ty == Tfunction && (cast(TypeFunction)type).iswild & 2)
                     v.storage_class |= STCreturn;
             }
-            if (type.ty == Tfunction && (cast(TypeFunction)type).isreturn)
-                v.storage_class |= STCreturn;
+            if (type.ty == Tfunction)
+            {
+                TypeFunction tf = cast(TypeFunction)type;
+                if (tf.isreturn)
+                    v.storage_class |= STCreturn;
+                if (tf.isscope)
+                    v.storage_class |= STCscope;
+            }
 
             v.semantic(sc);
             if (!sc.insert(v))
