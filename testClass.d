3e9e6ceb6f7179c9baefb1608c4e4ee8946ada84
Stefan Koch <uplink.coder@googlemail.com>
2018-03-31 10:25:25 +0200

    simplify test

--- testClass.d
+++ testClass.d
@@ -1,25 +1,18 @@
 class C 
 {
-  // this is ma base yo!
-  int base;
-
   int i() {return 1;}
 }
 
 class D : C
 {
- // this is ma' derived field yo!
- int d;
- override int i() {return 2;}
- float f() { return 1.0f; }
+  override int i() {return 2;}
+  float f() { return 1.0f; }
 }
 
 class E : D
 {
- // this is ma' derived field yo!
- double dbl;
- override int i() {return 3;}
- override float f() { return 2.0f; }
+  override int i() {return 3;}
+  override float f() { return 2.0f; }
 }
 
 
@@ -33,10 +26,7 @@ int testClassStuff ()
 
   D e = new E();
   assert(cast(int)e.f() == 2);
-//  pragma(msg, () {E e = new E(); return e.f(); } ());
 
   return c1.i + c2.i + c3.i;
 }
-
-
 static assert(testClassStuff == 1 + 2 + 3);
