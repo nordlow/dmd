fd67c28548b33ec9e61d8ce94b48c912cfd603a8
Stefan Koch <uplink.coder@googlemail.com>
2018-06-25 22:49:38 +0200

    Add Iterative binary search example

---
+++ ibs.d
@@ -0,0 +1,35 @@
+
+/// iterative binary search
+
+int ibs_find (const (uint[]) arr, uint v)
+{
+    return binarySearch(arr, 0, cast(int)(arr.length), v);
+}
+
+static immutable uint[] array = [1,3,5,7,11,13,17,21];
+
+int binarySearch(const (uint[]) arr, int l, int r, uint x)
+{
+    while (l <= r)
+    {
+        int m = l + (r-l)/2;
+ 
+        // Check if x is present at mid
+        if (arr[m] == x)
+            return m;
+ 
+        // If x greater, ignore left half
+        if (arr[m] < x)
+            l = m + 1;
+ 
+        // If x is smaller, ignore right half
+        else
+            r = m - 1;
+    }
+ 
+    // if we reach here, then element was
+    // not present
+    return -1;
+}
+ 
+static assert(ibs_find(array, 21) == 7);
